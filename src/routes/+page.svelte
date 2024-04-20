d87a5e5ee055d169e0435731eb430b05d4a5fa2e
jalzae <jalzae@gmail.com>
2024-04-20 15:01:12 +0700

    first config

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  let counter = 0;
+
+  const increment = (val: number = 0) => {
+    if (val > 0) {
+      counter += val;
+    } else {
+      counter++;
+    }
+  };
+  const decrement = () => {
+    counter++;
+  };
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<button on:click={() => increment()}>Count : {counter}</button>
+<button on:click={() => increment(10)}>Add 10</button>
